99e536c7abf9039981afc68819d5a1e55ac28972
Rafael Rego <rafael.rego@varsitytutors.com>
2025-03-23 23:04:01 -0300

    Improve 98. Validate Binary Search Tree

--- problems/trees/98-validate-binary-search-tree.py
+++ problems/trees/98-validate-binary-search-tree.py
@@ -1,7 +1,6 @@
 from typing import Optional
 
 
-# Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -27,3 +26,20 @@ class Solution:
             if values[i] >= values[i + 1]:
                 return False
         return True
+
+
+class Solution2:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return False
+
+        def validate(node, left_bound, right_bound):
+            if node is None:
+                return True
+
+            if not (left_bound < node.val < right_bound):
+                return False
+
+            return validate(node.left, left_bound, node.val) and validate(node.right, node.val, right_bound)
+
+        return validate(root, float('-inf'), float('inf'))
