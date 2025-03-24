a94b397c4c2b41e3805fc64bc06ab307970cb2e3
Rafael Rego <rafael.rego@varsitytutors.com>
2025-03-23 22:35:32 -0300

    improve Longest Repeating Character Replacement

--- problems/sliding-window/424-longest-repeating-character-replacement.py
+++ problems/sliding-window/424-longest-repeating-character-replacement.py
@@ -3,18 +3,9 @@ class Solution:
         def char_position(c):
             return ord(c) - ord('A')
 
-        def is_valid(arr):
-            tmp = k
-            max_val = float('-inf')
-            for i, x in enumerate(arr):
-                if x > max_val:
-                    max_val = x
-                    max_index = i
-
-            for i, x in enumerate(arr):
-                if i != max_index:
-                    tmp -= x
-            return tmp >= 0
+        def is_valid(arr, l, r):
+            max_f = max(arr)
+            return r - l + 1 - max_f <= k
 
         count = [0] * 26
         res = float('-inf')
@@ -22,7 +13,7 @@ class Solution:
         while r < len(s):
             c = s[r]
             count[char_position(c)] += 1
-            if is_valid(count):
+            if is_valid(count, l ,r):
                 res = max(res, r - l + 1)
                 r += 1
             else:
